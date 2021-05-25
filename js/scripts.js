34df989f2974dc036a8660f217848de297cd0f57
Ashley U <ashley.umetsu@gmail.com>
2021-05-25 16:08:49 -0500

    Changing profiles to only show city instead of adding state

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 const gallery = document.querySelector('.gallery'); 
-//Fetch function
+
+//Fetch function gets data from API
 
 fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
     .then(data => generateProfile(data.results))
 
-//Helper Functions
+//Function that loops through the data from the API and inserts it into employee profiles
 
 function generateProfile(data) {
     for(let i = 0; i < data.length; i++) {
@@ -17,7 +18,7 @@ function generateProfile(data) {
             <div class="card-info-container">
                 <h3 id="name" class="card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
                 <p class="card-text">${data[i].email}</p>
-                <p class="card-text cap">${data[i].location.city}, ${data[i].location.state}</p>
+                <p class="card-text cap">${data[i].location.city}</p>
             </div>
         </div>
         `;
