4a64f765854bf8cad9286edd55d6c5a62675b0a1
Ashley U <ashley.umetsu@gmail.com>
2021-05-25 16:03:48 -0500

    Displaying 12 API results on page

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
-
+const gallery = document.querySelector('.gallery'); 
 //Fetch function
 
-fetch('https://randomuser.me/api/')
+fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    .then(data => generateCard(data.results))
+    .then(data => generateProfile(data.results))
 
 //Helper Functions
 
-function generateCard(data) {
-    const html = `
+function generateProfile(data) {
+    for(let i = 0; i < data.length; i++) {
+        let profileHTML = `
         <div class="card">
             <div class="card-img-container">
-                <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+                <img class="card-img" src="${data[i].picture.medium}" alt="profile picture">
             </div>
             <div class="card-info-container">
-                <h3 id="name" class="card-name cap">first last</h3>
-                <p class="card-text">email</p>
-                <p class="card-text cap">city, state</p>
+                <h3 id="name" class="card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+                <p class="card-text">${data[i].email}</p>
+                <p class="card-text cap">${data[i].location.city}, ${data[i].location.state}</p>
             </div>
         </div>
-    `;
-
+        `;
+        gallery.insertAdjacentHTML("beforeend", profileHTML);
+    }
 }
