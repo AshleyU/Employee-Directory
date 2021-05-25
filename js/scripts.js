8096592c4624e60673de57eda73c3bcc89e7041d
Ashley U <ashley.umetsu@gmail.com>
2021-05-25 14:21:14 -0500

    Adding outline for generateCard function

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,22 @@
 
 fetch('https://randomuser.me/api/')
     .then(response => response.json())
-    .then(data => console.log(data.results))
+    .then(data => generateCard(data.results))
+
+//Helper Functions
+
+function generateCard(data) {
+    const html = `
+        <div class="card">
+            <div class="card-img-container">
+                <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+                <h3 id="name" class="card-name cap">first last</h3>
+                <p class="card-text">email</p>
+                <p class="card-text cap">city, state</p>
+            </div>
+        </div>
+    `;
+
+}
